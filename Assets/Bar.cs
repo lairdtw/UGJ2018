e0a8f7d5719811164a1ac02b10a04d8497174e7b
BuildTools <unconfigured@null.spigotmc.org>
2018-10-06 18:07:18 +0800

    no message

---
+++ Assets/Bar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour {
+    public static double Food=0, Goal=100;
+    public Image cooldown;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cooldown.fillAmount += 1.0f / 30 * Time.deltaTime;
+    }
+}
