85b1bd82856f3b98b324a41f7f72184f5d5045cc
Laird <laird@goonsdesign.com>
2018-10-07 16:49:08 +0800

    Add End

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    public AudioSource LoopMusic;
+
+	// Use this for initialization
+	void Start () {
+        LoopMusic.clip = Resources.Load<AudioClip>("Music/Logo");
+        LoopMusic.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0)){
+            SceneManager.LoadScene(1);
+        }
+	}
+}
